60ad4978de0cf9a556fab551c7b708a1c72b2c3c
Kenshi <vklapshov@gmail.com>
2025-07-02 23:14:07 +0300

    react compiler + preload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     base: "/shri2025-task/",
-    plugins: [react()],
+    plugins: [
+        react(),
+        {
+            babel: {
+                plugins: [["babel-plugin-react-compiler"]],
+            },
+        },
+    ],
 });
