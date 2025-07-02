efd6e0cf8299da15a4bf7f82710d94dc6c20f7a6
Kenshi <vklapshov@gmail.com>
2025-07-02 23:41:49 +0300

    moved header from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <MainContent />
-        </>
-    );
+    return <MainContent />;
 }
 
 export default App;
