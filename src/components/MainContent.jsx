b0542fd17253d87ec218b510122bd37b0bfafe2c
Kenshi <vklapshov@gmail.com>
2025-07-02 22:50:06 +0300

    fixed list logic

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -15,7 +15,7 @@ function MainContent() {
                 new URLSearchParams(location.search).get("tab") || "hall"
             );
         }
-    });
+    }, [activeTab]);
 
     const onSelectInput = (event) => {
         setActiveTab(event.target.value);
@@ -28,7 +28,6 @@ function MainContent() {
 
     React.useEffect(() => {
         const sumWidth = sizes.reduce((acc, item) => acc + item.width, 0);
-        const sumHeight = sizes.reduce((acc, item) => acc + item.height, 0);
 
         const newHasRightScroll = sumWidth > ref.current.offsetWidth;
         if (newHasRightScroll !== hasRightScroll) {
@@ -195,31 +194,19 @@ function MainContent() {
                 </div>
 
                 <div className="section__panel-wrapper" ref={ref}>
-                    {TABS_KEYS.map((key) => (
-                        <div
-                            key={key}
-                            role="tabpanel"
-                            className={
-                                "section__panel" +
-                                (key === activeTab
-                                    ? ""
-                                    : " section__panel_hidden")
-                            }
-                            aria-hidden={key === activeTab ? "false" : "true"}
-                            id={`panel_${key}`}
-                            aria-labelledby={`tab_${key}`}
-                        >
-                            <ul className="section__panel-list">
-                                {TABS[key].items.map((item, index) => (
-                                    <Event
-                                        key={index}
-                                        {...item}
-                                        onSize={onSize}
-                                    />
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
+                    <div
+                        role="tabpanel"
+                        className="section__panel"
+                        aria-hidden="false"
+                        id={`panel_${activeTab}`}
+                        aria-labelledby={`tab_${activeTab}`}
+                    >
+                        <ul className="section__panel-list">
+                            {TABS[activeTab]?.items.map((item, index) => (
+                                <Event key={index} {...item} onSize={onSize} />
+                            ))}
+                        </ul>
+                    </div>
                     {hasRightScroll && (
                         <div
                             className="section__arrow"
